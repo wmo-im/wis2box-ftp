5a184723e8a41e762f77d92cb4289592da338a6f
Maaike <maaike.limper@gmail.com>
2022-11-17 08:58:32 +0100

    initial-version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # minio-ftp-forwarder
 ftp forwarding files to MinIO endpoint
+
+## how to use
+
+Update the contents of env_file and then start with
+
+```bash
+docker-compose -f docker-compose-minio-ftp.yml --env-file env_file 
+```
---
+++ docker/incoming-ftp/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+addgroup \
+	-g $GID \
+	-S \
+	$FTP_USER
+
+adduser \
+	-D \
+	-G $FTP_USER \
+	-h /data/incoming \
+	-s /bin/false \
+	-u $UID \
+	$FTP_USER
+
+mkdir -p /data/incoming
+chown -R $FTP_USER:$FTP_USER /data/incoming
+echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+
+touch /var/log/vsftpd.log
+tail -f /var/log/vsftpd.log | tee /dev/stdout &
+touch /var/log/xferlog
+tail -f /var/log/xferlog | tee /dev/stdout &
+
+exec "$@"
